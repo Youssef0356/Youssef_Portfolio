86193221b5157ac6021721473ca434cb8f91f5f5
Youssef0356 <nejiyoussef081@gmail.com>
2023-12-19 13:59:59 +0100

    added button for cv

---
+++ JS/MainPage.js
@@ -0,0 +1,4 @@
+document.addEventListener("mousemove", (e) => {
+    const cursor = document.getElementById("custom-cursor");
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  });
