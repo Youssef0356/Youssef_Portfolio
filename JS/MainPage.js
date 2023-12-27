013f977960194545f23a76e65dacd052477706b6
Youssef0356 <nejiyoussef081@gmail.com>
2023-12-27 10:11:54 +0100

    .

--- JS/MainPage.js
+++ JS/MainPage.js
@@ -2,3 +2,6 @@ document.addEventListener("mousemove", (e) => {
     const cursor = document.getElementById("custom-cursor");
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
   });
+function gamemaking(){
+alert("pressed!")
+}
