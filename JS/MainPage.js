7779c3e4c6072c2cf9d8e3ac353f64303820dfec
Youssef0356 <nejiyoussef081@gmail.com>
2024-01-09 12:12:58 +0100

    i did alot of modifications

--- JS/MainPage.js
+++ JS/MainPage.js
@@ -2,6 +2,3 @@ document.addEventListener("mousemove", (e) => {
     const cursor = document.getElementById("custom-cursor");
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
   });
-function gamemaking(){
-alert("pressed!")
-}
